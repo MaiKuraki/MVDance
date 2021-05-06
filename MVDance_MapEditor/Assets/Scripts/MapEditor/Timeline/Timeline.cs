11f87049f93668c49c8bbc45893497a9fe39c316
MaiKuraki <mai.k@live.com>
2021-05-07 04:14:18 +0800

    update prefab

--- MVDance_MapEditor/Assets/Scripts/MapEditor/Timeline/Timeline.cs
+++ MVDance_MapEditor/Assets/Scripts/MapEditor/Timeline/Timeline.cs
@@ -6,71 +6,92 @@ using RyanNielson.InputBinder;
 
 namespace MVDance.MapEditor
 {
+    public enum TimelineType
+    {
+        Second,
+        Beat,
+        Frame
+    }
+
     public class Timeline : MonoBehaviour
     {
         [Header("-- Timeline Config --")]
         [SerializeField] TimelinePanel timeline_panel;
         [SerializeField] TimelineHandle timeline_handle;
-        [SerializeField] UIGridRenderer bottom_timeline_grid_main;
-        [SerializeField] UIGridRenderer bottom_timeline_grid_sub;
-        [SerializeField] Scrollbar bottom_timeline_progress_bar;
+        [SerializeField] UIGridRenderer grid_main;
+        [SerializeField] UIGridRenderer grid_sub;
+        [SerializeField] Transform timeline_bar;
         [SerializeField] Text time_text;
         [SerializeField] Text time_text_copy;
-        [SerializeField] Transform timeline_grid_value_root;
-        [SerializeField] Transform timeline_grid_value_text_tr;
+        [SerializeField] Transform grid;
+        [SerializeField] Transform grid_value_root;
+        [SerializeField] Transform grid_value_text_tr;
 
         [Header("-- Test Var --")]
         [SerializeField] double mainGridTotalTime = 10;
 
 
-
+        Scrollbar timeline_bar_scroll;
         InputBinder inputComponent;
+        RectTransform timeline_bar_rt;
+        RectTransform grid_rt;
+
 
         bool isMouseHoverOnTimeline = false;
         bool canRunTimeline = true;
         Action Action_OnTimelineScaled;
+        Action Action_OnTimelinePreScale;
+        TimelineType eTimelineType = TimelineType.Second;
 
         private void Awake()
         {
+            timeline_bar_scroll = timeline_bar.GetComponent<Scrollbar>();
+            timeline_bar_rt = timeline_bar.GetComponent<RectTransform>();
+            grid_rt = grid.GetComponent<RectTransform>();
+
             timeline_handle.Action_OnDragging += d =>
             {
                 //  do not remove.
-                bottom_timeline_progress_bar.OnDrag(d);
+                timeline_bar_scroll.OnDrag(d);
             };
             timeline_handle.Action_OnPointerDown += d =>
             {
                 //  hold is drag, hold is pause
-                dragStartVal = bottom_timeline_progress_bar.value;
+                dragStartVal = timeline_bar_scroll.value;
                 UpdateTimelineData(false);
             };
             timeline_handle.Action_OnPointerUp += d =>
             {
                 //  hold is drag, hold is pause
-                dragEndVal = bottom_timeline_progress_bar.value;
+                dragEndVal = timeline_bar_scroll.value;
                 dragOffset_Sec += mainGridTotalTime * (dragEndVal - dragStartVal);
                 UpdateTimelineData(true);
             };
 
-            bottom_timeline_progress_bar.onValueChanged.AddListener(f =>
+            timeline_bar_scroll.onValueChanged.AddListener(f =>
             {
-                UpdateTimelineHandleText(0, (long)(f * mainGridTotalTime * 1000));
+                UpdateTimelineHandleText((long)(f * mainGridTotalTime * 1000));
             });
             Action_OnTimelineScaled += () =>
             {
-                UpdateTimelineHandleText(0, (long)(bottom_timeline_progress_bar.value * mainGridTotalTime * 1000));
+                UpdateTimelineHandleText((long)(timeline_bar_scroll.value * mainGridTotalTime * 1000));
                 RefreshGridValue();
+                TryLockGridToCursor();
+            };
+            Action_OnTimelinePreScale += () =>
+            {
+                TryLockGridToCursor();
             };
             RefreshGridValue();
-            UpdateTimelineHandleText(0, (long)(bottom_timeline_progress_bar.value * mainGridTotalTime * 1000));
+            UpdateTimelineHandleText((long)(timeline_bar_scroll.value * mainGridTotalTime * 1000));
 
             timeline_panel.Action_OnPointerEnter += () => isMouseHoverOnTimeline = true;
             timeline_panel.Action_OnPointerExit += () => isMouseHoverOnTimeline = false;
 
             inputComponent = GetComponent<InputBinder>();
             inputComponent.BindAxis("Mouse ScrollWheel", TryScaleTimeline);
-            inputComponent.BindKey(KeyCode.R, InputEvent.Pressed, TryRunTimelineFromStart);
+            inputComponent.BindKey(KeyCode.R, InputEvent.Pressed, ()=> { TryRunTimelineFromStart(); runningBeforePause = true; });
             inputComponent.BindKey(KeyCode.Space, InputEvent.Pressed, () => { UpdateTimelineData(!canRunTimeline); });
-
 
             InitTimelineGrid((int)mainGridTotalTime);
         }
@@ -85,6 +106,7 @@ namespace MVDance.MapEditor
             else
             {
                 pauseStartTime = DateTime.Now.Ticks;
+                runningBeforePause = false;
             }
 
             totalPausedTime += pauseEndTime - pauseStartTime > 0 ? pauseEndTime - pauseStartTime : 0;
@@ -93,10 +115,12 @@ namespace MVDance.MapEditor
         long GetPausedTime() => totalPausedTime;
         bool CanRunTimeline() => canRunTimeline;
         bool CanScale() => isMouseHoverOnTimeline;
+        bool IsTimelineRunning() => pauseStartTime < pauseEndTime || runningBeforePause;
         private void TryScaleTimeline(float mouseWheelVal)
         {
             if (CanScale() && (mouseWheelVal > 0.01f || mouseWheelVal < -0.01f))
             {
+                Action_OnTimelinePreScale?.Invoke();
                 if (mouseWheelVal > 0.01f)
                 {
                     ScaleUpTimeline();
@@ -115,12 +139,15 @@ namespace MVDance.MapEditor
         float dragStartVal = 0;
         float dragEndVal = 0;
         double dragOffset_Sec = 0;
+        bool runningBeforePause = false;
+        long timelineStartVal = 0;
+        long timelineEndval = 0;
 
         Coroutine task_updateTime = null;
 
         private void ResetTimeline()
         {
-            bottom_timeline_progress_bar.value = 0;
+            timeline_bar_scroll.value = 0;
             totalPausedTime = 0;
             dragOffset_Sec = 0;
             canRunTimeline = true;
@@ -151,16 +178,16 @@ namespace MVDance.MapEditor
                 TimeSpan ts = new TimeSpan(DateTime.Now.Ticks - GetPausedTime() - startTime);
                 // UpdateTimelineHandleText(0,(long)((ts.TotalSeconds + GetDragOffset_Seconds()) * 1000));
                 progress = (ts.TotalSeconds + GetDragOffset_Seconds()) / mainGridTotalTime;
-                bottom_timeline_progress_bar.value = (float)progress;
+                timeline_bar_scroll.value = (float)progress;
                 yield return new WaitUntil(CanRunTimeline);
             }
-            bottom_timeline_progress_bar.value = Mathf.Clamp((float)progress, 0, 1);
+            timeline_bar_scroll.value = Mathf.Clamp((float)progress, 0, 1);
         }
 
-        void UpdateTimelineHandleText(int displayType, long inTime)
+        void UpdateTimelineHandleText(long inTime)
         {
             string displayStr = "NULL";
-            if (displayType == 0)
+            if (eTimelineType == TimelineType.Second)
             {
                 displayStr = string.Format("{0:00.00}", (inTime / 1000.0f).ToString("f2"));
             }
@@ -168,58 +195,91 @@ namespace MVDance.MapEditor
             time_text_copy.text = displayStr;
         }
 
+        float gridScaleRatio = 1;
         void ScaleUpTimeline()
         {
-            bottom_timeline_grid_main.gridSize = new Vector2Int(Mathf.Clamp(bottom_timeline_grid_main.gridSize.x - 1, 1, 20), 1);
-            bottom_timeline_grid_main.gameObject.SetActive(false);
-            bottom_timeline_grid_main.gameObject.SetActive(true);
+            //float startScaleSizeX = Mathf.Clamp(grid_main.gridSize.x, 1, 20);
+            //float endScaleSizeX = Mathf.Clamp(grid_main.gridSize.x - 1, 1, 20);
 
-            bottom_timeline_grid_sub.gridSize = new Vector2Int(Mathf.Clamp(bottom_timeline_grid_sub.gridSize.x - 10, 10, 200), 1);
-            bottom_timeline_grid_sub.gameObject.SetActive(false);
-            bottom_timeline_grid_sub.gameObject.SetActive(true);
+            //gridScaleRatio = (startScaleSizeX / endScaleSizeX);
+            //print($"ratio{gridScaleRatio}");
+            //float sourceGridWidth = grid_rt.rect.width;
+            //float sourceGridHeight = grid_rt.rect.height;
+            //float targetWidth = sourceGridWidth * gridScaleRatio;
+            //print($"targetWidth: {targetWidth}");
+            //grid_rt.sizeDelta = new Vector2(targetWidth, sourceGridHeight);
 
-            mainGridTotalTime = bottom_timeline_grid_main.gridSize.x;
+            grid_main.gridSize = new Vector2Int(Mathf.Clamp(grid_main.gridSize.x - 1, 1, 20), 1);
+            grid_main.gameObject.SetActive(false);
+            grid_main.gameObject.SetActive(true);
+
+            grid_sub.gridSize = new Vector2Int(Mathf.Clamp(grid_sub.gridSize.x - 10, 10, 200), 1);
+            grid_sub.gameObject.SetActive(false);
+            grid_sub.gameObject.SetActive(true);
+
+            mainGridTotalTime = grid_main.gridSize.x;
         }
         void ScaleDownTimeline()
         {
-            bottom_timeline_grid_main.gridSize = new Vector2Int(Mathf.Clamp(bottom_timeline_grid_main.gridSize.x + 1, 1, 20), 1);
-            bottom_timeline_grid_main.gameObject.SetActive(false);
-            bottom_timeline_grid_main.gameObject.SetActive(true);
+            grid_main.gridSize = new Vector2Int(Mathf.Clamp(grid_main.gridSize.x + 1, 1, 20), 1);
+            grid_main.gameObject.SetActive(false);
+            grid_main.gameObject.SetActive(true);
 
-            bottom_timeline_grid_sub.gridSize = new Vector2Int(Mathf.Clamp(bottom_timeline_grid_sub.gridSize.x + 10, 10, 200), 1);
-            bottom_timeline_grid_sub.gameObject.SetActive(false);
-            bottom_timeline_grid_sub.gameObject.SetActive(true);
+            grid_sub.gridSize = new Vector2Int(Mathf.Clamp(grid_sub.gridSize.x + 10, 10, 200), 1);
+            grid_sub.gameObject.SetActive(false);
+            grid_sub.gameObject.SetActive(true);
 
-            mainGridTotalTime = bottom_timeline_grid_main.gridSize.x;
+            mainGridTotalTime = grid_main.gridSize.x;
         }
         void InitTimelineGrid(int mainGridNum)
         {
-            bottom_timeline_grid_main.gridSize = new Vector2Int(mainGridNum, 1);
-            bottom_timeline_grid_main.gameObject.SetActive(false);
-            bottom_timeline_grid_main.gameObject.SetActive(true);
+            grid_main.gridSize = new Vector2Int(mainGridNum, 1);
+            grid_main.gameObject.SetActive(false);
+            grid_main.gameObject.SetActive(true);
 
-            bottom_timeline_grid_sub.gridSize = new Vector2Int(mainGridNum * 10, 1);
-            bottom_timeline_grid_sub.gameObject.SetActive(false);
-            bottom_timeline_grid_sub.gameObject.SetActive(true);
+            grid_sub.gridSize = new Vector2Int(mainGridNum * 10, 1);
+            grid_sub.gameObject.SetActive(false);
+            grid_sub.gameObject.SetActive(true);
         }
         void RefreshGridValue()
         {
-            RectTransform grid_rt = timeline_grid_value_root.GetComponent<RectTransform>();
-            for (int i = 0; i < timeline_grid_value_root.childCount; i++)
+            for (int i = 0; i < grid_value_root.childCount; i++)
             {
-                Destroy(timeline_grid_value_root.GetChild(i).gameObject);
+                Destroy(grid_value_root.GetChild(i).gameObject);
             }
 
             for (int i = 0; i < (int)mainGridTotalTime + 1; i++)
             {
-                Transform t = Instantiate(timeline_grid_value_text_tr);
+                Transform t = Instantiate(grid_value_text_tr);
                 t.GetComponent<Text>().text = i.ToString();
-                t.SetParent(timeline_grid_value_root);
+                t.SetParent(grid_value_root);
                 t.localScale = Vector3.one;
                 float posX = mainGridTotalTime > 0 ? i * grid_rt.rect.size.x / (float)mainGridTotalTime - grid_rt.rect.size.x / 2.0f : 0;
                 // print($"POSX: {posX}");
                 t.localPosition = new Vector2(posX, -10);
             }
+        }
+
+        void TryLockGridToCursor()
+        {
+            if (IsTimelineRunning())
+            {
+                Debug.Log("Timeline running, skip lock");
+                return;
+            }
+
+            print($"timelineBar: {timeline_bar_rt.rect.width}, Grid: {grid_rt.rect.width}");
+        }
+
+        long GetTimelineValue()
+        {
+            long result = 0;
+            if (eTimelineType == TimelineType.Second)
+            {
+                long timelineTotalval = timelineEndval - timelineStartVal;
+                result = timelineStartVal + (long)(timeline_bar_scroll.value * timelineTotalval);
+            }
+            return result;
         }
     }
 }
